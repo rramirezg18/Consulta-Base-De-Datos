b1d0fef82ab1c63367b09b67689a604d8a47c3c4
Roberto Ramirez <rramirezg18@miumg.edu.gt>
2023-10-14 15:50:21 -0600

    first commit

---
+++ src/Cliente.java
@@ -0,0 +1,25 @@
+public class Cliente {
+    private String nit;
+    private String nombre;
+
+    public Cliente(String nit, String nombre) {
+        this.nit = nit;
+        this.nombre = nombre;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
